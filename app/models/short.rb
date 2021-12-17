d3071fae5a8dc3f4ee304f96ba2706c5dad69a15
mazabud <maz.abud@gmail.com>
2021-12-17 16:53:22 +0700

    update model

--- app/models/short.rb
+++ app/models/short.rb
@@ -4,7 +4,7 @@ class Short < ApplicationRecord
     #validates :url, format: URI::regexp(%w[http https])
     validates_uniqueness_of :shortcode, message: "The the desired shortcode is already in use. Shortcodes are case-sensitive.", status: 409
     # validates_length_of :url, within: 3..255, on: :create, message: "too long"
-    validates_format_of :shortcode, with: /((\w+\s?-?\w+)(,|\z))/i, on: :create, message: "The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{6}$.", status: 422 
+    validates_format_of :shortcode, with: /\b[A-Z0-9]{6}\b/i, on: :create, message: "The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{6}$.", status: 422 
     
    
     # the API
